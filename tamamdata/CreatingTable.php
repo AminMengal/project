9d55eaefb940af1907ccca014dd7ce36c29a6765
AminMengal <ameendgamm@gmail.com>
2018-03-05 22:48:38 +0500

    Initial commit

---
+++ tamamdata/CreatingTable.php
@@ -0,0 +1,51 @@
+<?php
+include 'connectivitydata.php';
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // sql to create table
+    $sql = "CREATE TABLE MyGuest (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    Company VARCHAR(200) NOT NULL,
+    images VARCHAR(200) NOT NULL,
+    email text(500),
+    reg_date TIMESTAMP
+    )";
+
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Table MyGuests created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
